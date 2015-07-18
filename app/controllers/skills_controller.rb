82bc66e43915e111ac7e62df7bd58a1694aa519b
steven francisco <stvnfrancisco@yahoo.com>
2015-07-18 12:22:49 -0700

    add create function to skills_controller.rb file.... allows user to create and add skills to the list of skills

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -7,10 +7,24 @@ class SkillsController < ApplicationController
   def new
   @skill = Skill.new
   render :new
-end  
+end
+
+def create
+  @skill = Skill.new(skill_params)
+  if @skill.save
+    redirect_to  skills_path
+  else
+    render :new
+  end
+end
 
     def show
     @skill = Skill.find(params[:id])
     render :show
   end
 end
+
+private
+  def skill_params
+    params.require(:skill).permit(:name)
+  end
