cea5baa9010fbf9dfd61bc39a371982d42acf2a5
steven francisco <stvnfrancisco@yahoo.com>
2015-07-18 13:57:01 -0700

    add edit and delete functions to skills

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,30 +1,50 @@
 class SkillsController < ApplicationController
   def index
     @skills = Skill.all
-    render :index
   end
+
+  def show
+  @skill = Skill.find(params[:id])
+end
 
   def new
   @skill = Skill.new
-  render :new
 end
 
 def create
   @skill = Skill.new(skill_params)
   if @skill.save
-    redirect_to  skills_path
-  else
-    render :new
+    flash[:notice] = "Skill successfully added!"
+      redirect_to skills_path
+    else
+      render :new
+    end
   end
-end
 
-    def show
+  def edit
     @skill = Skill.find(params[:id])
-    render :show
   end
-end
+
+  def update
+    @skill = Skill.find(params[:id])
+    if @skill.update(skill_params)
+      flash[:notice] = "Skill successfully updated!"
+      redirect_to skill_path(@skill)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @skill = Skill.find(params[:id])
+    @skill.destroy
+    flash[:notice] = "Skill successfully deleted."
+    redirect_to skills_path
+  end
 
 private
   def skill_params
-    params.require(:skill).permit(:name)
+    params.require(:skill).permit(:name, :description)
   end
+
+end
--- spec/models/skill_spec.rb
+++ spec/models/skill_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
 describe Skill do
-  it { should validate_presence_of :name }
+  it { should validate_presence_of :name}
 end
