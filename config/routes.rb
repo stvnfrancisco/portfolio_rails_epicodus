6045ec9bddf2c204d0bfdd2a2e39c0a0985b1358
steven francisco <stvnfrancisco@yahoo.com>
2015-07-20 13:41:07 -0700

    edits

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
-
+  
  root 'skills#index'
 
-
-  resources :abouts, :only => [:index, :show] 
+  resources :abouts, :only => [:index, :show]
   resources :skills do
     resources :experiences, :except => [:index, :show]
   resources :projects, :only => [:index] do
-    resources :github, :only => [:index]          
+    resources :github, :only => [:index]
     end
   end
 end
